ada8b1ad9ccdd415efd093658a026547c570282f
Michel Adasme <mic.adasme@gmail.com>
2023-07-31 23:20:55 -0400

    feat(app): nuevas tiendas y nuevo estilo
    CAMBIOS

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
+import urllib.parse
 from urllib.parse import quote_plus
 
 app = Flask(__name__)
 
 def buscar_alimento(url, selectors):
+    print(url)
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
+        # print(soup)
         resultado = soup.select_one(selectors['container'])
         if resultado:
             nombre_elemento = resultado.select_one(selectors['nombre'])
             precio_elemento = resultado.select_one(selectors['precio'])
-            stock_elemento = resultado.select_one(selectors['stock'])
+            stock_elemento = resultado.select_one(selectors['stock']) if selectors['stock'] else ''
 
             nombre = nombre_elemento.text.strip() if nombre_elemento else 'Sin Informacion'
             precio = precio_elemento.text.strip() if precio_elemento else 'Sin Informacion'
@@ -24,7 +27,7 @@ def buscar_alimento(url, selectors):
             else:
                 stock = "Con Stock"
             
-            return {"nombre": nombre, "precio": precio, "stock": stock}
+            return {"shop":selectors['shop'], "nombre": nombre, "precio": precio, "stock": stock}
     
     return None
 
@@ -37,93 +40,124 @@ def index():
     resultado_tusmascotas_full = None
     resultado_novapet = None
     resultado_mlandia = None
+    resultado_mlandia_full = None
     resultado_gpet = None
     resultado_dgppet = None
     resultado_stgopet = None
     resultado_lira = None
+    resultado_lira_full = None
     resultado_faunimals = None
     resultado_manchasSofi = None
     nombre_alimento = None
 
     if request.method == 'POST':
         nombre_alimento = request.form['nombre_alimento']
+        nombre_alimento_codificado = urllib.parse.quote(nombre_alimento)
         nombre_alimento_mas = nombre_alimento.replace(" ", "+")
-        nombre_alimento_codificado = quote_plus(nombre_alimento_mas)
-
+        nombre_alimento_mas_codificado = quote_plus(nombre_alimento_mas)
+        nombre_codificado_safe_plus = urllib.parse.quote(nombre_alimento_mas, safe='+')
+        nombre_alimento_mas_cast_y = nombre_alimento_mas.replace("&", "y")
         # Configuración de selectores para cada sitio web
         selectors_puntomascotas = {
-            'container': "div.product-list",
-            'nombre': "a",
+            'shop': "PuntoMascotas",
+            'container': "article.product-miniature.js-product-miniature",
+            'nombre': "h5.product-name",
             'precio': "span.price.product-price",
             'stock': "div.product-availability"
         }
         selectors_braloy = {
+            'shop': "Braloy",
             'container': "article.product-miniature-default",
             'nombre': "h3.product-title",
             'precio': "span.product-price",
             'stock': "div.product-availability"
         }
         selectors_petslandia = {
+            'shop': "Petslandia",
             'container': "div.product-wrapper",
             'nombre': "h3.wd-entities-title",
             'precio': "span.amount",
             'stock': "div.product-labels.labels-rectangular"
         }
         selectors_tusmascotas = {
+            'shop': "TusMascotas",
             'container': "div.product-grid-item",
             'nombre': "h3.wd-entities-title",
             'precio': "span.woocommerce-Price-amount.amount",
             'stock': "span.out-of-stock"
         }
         selectors_tusmascotas_full = {
+            'shop': "TusMascotas",
             'container': "div.container-fluid",
             'nombre': "h1.product_title.entry-title",
             'precio': "span.woocommerce-Price-amount.amount",
             'stock': "span.out-of-stock.product-label"
         }
         selectors_novapet = {
+            'shop': "Novapet",
             'container': "div.bs-collection__product.border",
             'nombre': "h3.bs-collection__product-title",
             'precio': "strong.bs-collection__product-final-price",
             'stock': "button.btn.btn-primary"
         }
         selectors_mlandia = {
+            'shop': "Mlandia",
             'container': "div.product-grid-item.product",
             'nombre': "h3.product-title",
             'precio': "span.woocommerce-Price-amount.amount",
             'stock': "span.out-of-stock"
         }
+        selectors_mlandia_full = {
+            'shop': "Mlandia 2",
+            'container': "div.row.product-image-summary-wrap",
+            'nombre': "h1.product_title.entry-title",
+            'precio': "ins",
+            'stock': "p.stock.out-of-stock"
+        }
         selectors_gpet = {
+            'shop': "Gpet",
             'container': "td.oe_product",
-            'nombre': "a.tp-link",
+            'nombre': "h6.o_wsale_products_item_title",
             'precio': "span.oe_currency_value",
             'stock': "span.out-of-stock"
         }
         selectors_dgppet = {
+            'shop': "De Gatos y Perros",
             'container': "div.row.bs-collection",
             'nombre': "h3.bs-collection__product-title",
             'precio': "strong.bs-collection__product-final-price",
             'stock': "div.bs-collection__product-stock"
         }
         selectors_stgopet = {
-            'container': "div.product-thumb",
-            'nombre': "a",
+            'shop': "Stgopet",
+            'container': "div.row.products-category",
+            'nombre': "h4",
             'precio': "p.price",
             'stock': ""
         }
         selectors_lira = {
+            'shop': "Distribuidora Lira",
             'container': "div.shop-container",
             'nombre': "a.woocommerce-LoopProduct-link.woocommerce-loop-product__link",
             'precio': "span.woocommerce-Price-amount.amount",
             'stock': "div.out-of-stock-label"
         }
+        selectors_lira_full = {
+            'shop': "Distribuidora Lira 2",
+            'container': "div.product-info.summary",
+            'nombre': "h1.product-title",
+            'precio': "span.woocommerce-Price-amount.amount",
+            'stock': "p.stock.out-of-stock"
+        }
         selectors_faunimals = {
+            'shop': "Faunimals",
             'container': "div.d-flex.col-sm-6.col-md-3.text-center",
             'nombre': "h6.text-truncate",
             'precio': "div.bs-product-final-price",
             'stock': ""
         }
         selectors_manchasSofi = {
+            'shop': "Las Manchas de Sofi",
             'container': "div.products.elements-grid.wd-products-holder",
             'nombre': "h3.wd-entities-title",
             'precio': "span.woocommerce-Price-amount.amount",
@@ -131,23 +165,26 @@ def index():
         }
 
         # Realizar búsquedas en los sitios web
-        resultado_puntomascotas = buscar_alimento(f"https://puntomascotas.cl/buscar?controller=search&s={nombre_alimento_mas}", selectors_puntomascotas)
-        resultado_braloy = buscar_alimento(f"https://braloy.cl/busqueda?controller=search&s={nombre_alimento_mas}", selectors_braloy)
+        
+        resultado_puntomascotas = buscar_alimento(f"https://puntomascotas.cl/buscar?controller=search&s={nombre_codificado_safe_plus}", selectors_puntomascotas)
+        resultado_braloy = buscar_alimento(f"https://braloy.cl/busqueda?controller=search&s={nombre_alimento_mas_cast_y}", selectors_braloy)
         resultado_petslandia = buscar_alimento(f"https://www.petslandia.cl/?s={nombre_alimento_mas}&post_type=product", selectors_petslandia)
         resultado_tusmascotas = buscar_alimento(f"https://www.tusmascotas.cl/?s={nombre_alimento_mas}&post_type=product", selectors_tusmascotas)
         resultado_tusmascotas_full = buscar_alimento(f"https://www.tusmascotas.cl/?s={nombre_alimento_mas}&post_type=product", selectors_tusmascotas_full)
-        resultado_novapet = buscar_alimento(f"https://www.novapet.cl/search?search_text={nombre_alimento_codificado}&limit=24&order=relevance&way=DESC", selectors_novapet)
+        resultado_novapet = buscar_alimento(f"https://www.novapet.cl/search?search_text={nombre_alimento_mas_codificado}&limit=24&order=relevance&way=DESC", selectors_novapet)
         resultado_mlandia = buscar_alimento(f"https://mlandia.cl/?s={nombre_alimento_mas}&post_type=product", selectors_mlandia)
+        resultado_mlandia_full = buscar_alimento(f"https://mlandia.cl/?s={nombre_alimento_mas}&post_type=product", selectors_mlandia_full)
         resultado_gpet = buscar_alimento(f"https://gpet.cl/shop?search={nombre_alimento_mas}", selectors_gpet)
-        resultado_dgppet = buscar_alimento(f"https://www.dgppet.cl/search?search_text={nombre_alimento_codificado}&limit=12&order=relevance&way=DESC", selectors_dgppet)
+        resultado_dgppet = buscar_alimento(f"https://www.dgppet.cl/search?search_text={nombre_alimento_mas_codificado}&limit=12&order=relevance&way=DESC", selectors_dgppet)
         resultado_stgopet = buscar_alimento(f"https://www.santiagopetstore.cl/tienda/index.php?route=product/search&search={nombre_alimento_codificado}", selectors_stgopet)
         resultado_lira = buscar_alimento(f"https://www.distribuidoralira.cl/?product_cat=&s={nombre_alimento_mas}&post_type=product", selectors_lira)
-        # resultado_faunimals = buscar_alimento(f"https://www.faunimals.com/search?search_text={nombre_alimento_codificado}&limit=24&order=relevance&way=DESC&page=1", selectors_faunimals)
+        resultado_lira_full = buscar_alimento(f"https://www.distribuidoralira.cl/?product_cat=&s={nombre_alimento_mas}&post_type=product", selectors_lira_full)
+        resultado_faunimals = buscar_alimento(f"https://www.faunimals.com/search?search_text={nombre_alimento_mas_codificado}&limit=24&order=relevance&way=DESC&page=1", selectors_faunimals)
         resultado_manchasSofi = buscar_alimento(f"https://lasmanchasdesofi.cl/?s={nombre_alimento_mas}&post_type=product", selectors_manchasSofi)
 
     return render_template('index.html', nombre=nombre_alimento, resultado_puntomascotas=resultado_puntomascotas, resultado_braloy=resultado_braloy, resultado_petslandia=resultado_petslandia, resultado_tusmascotas=resultado_tusmascotas,
-    resultado_tusmascotas_full=resultado_tusmascotas_full, resultado_novapet=resultado_novapet, resultado_mlandia=resultado_mlandia, resultado_gpet=resultado_gpet, resultado_dgppet=resultado_dgppet, resultado_stgopet=resultado_stgopet, 
-    resultado_lira=resultado_lira, resultado_faunimals=resultado_faunimals, resultado_manchasSofi=resultado_manchasSofi)
+    resultado_tusmascotas_full=resultado_tusmascotas_full, resultado_novapet=resultado_novapet, resultado_mlandia=resultado_mlandia, resultado_mlandia_full=resultado_mlandia_full, resultado_gpet=resultado_gpet, resultado_dgppet=resultado_dgppet, resultado_stgopet=resultado_stgopet, 
+    resultado_lira=resultado_lira, resultado_lira_full=resultado_lira_full, resultado_faunimals=resultado_faunimals, resultado_manchasSofi=resultado_manchasSofi)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
